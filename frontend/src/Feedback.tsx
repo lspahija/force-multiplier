a4164e63d69ec7c0e92f18404c972692a3e619f1
lspahija <lspahijaoec@gmail.com>
2023-06-09 16:46:51 -0500

    feedback

--- frontend/src/Feedback.tsx
+++ frontend/src/Feedback.tsx
@@ -1,5 +1,7 @@
-import {Title, Text, createStyles, rem, Container} from '@mantine/core';
+import {Title, Text, createStyles, rem, Container, Loader} from '@mantine/core';
 import {useLocation} from "react-router-dom";
+import {useState} from "react";
+import {useMicVAD, utils} from "@ricky0123/vad-react";
 
 const useStyles = createStyles((theme) => ({
     inner: {
@@ -13,7 +15,7 @@ const useStyles = createStyles((theme) => ({
         },
     },
     title: {
-       paddingBottom: rem(40)
+        paddingBottom: rem(40)
     }
 }));
 
@@ -22,10 +24,99 @@ export function Feedback() {
 
     const location = useLocation()
     const {document} = location.state
+    const [currentDocument, setCurrentDocument] = useState<string>(document);
+    const [isSpeaking, setIsSpeaking] = useState(false)
+    const [isProcessing, setIsProcessing] = useState(false);
+
+    useMicVAD({
+        preSpeechPadFrames: 5,
+        positiveSpeechThreshold: 0.90,
+        negativeSpeechThreshold: 0.75,
+        minSpeechFrames: 4,
+        startOnLoad: true,
+        onSpeechStart: () => {
+            console.log("speech started")
+            setIsSpeaking(true)
+        },
+        onSpeechEnd: async (audio) => {
+            console.log("speech ended")
+            setIsSpeaking(false)
+            await processAudio(audio);
+        },
+        onVADMisfire: () => {
+            console.log("VAD misfire")
+            setIsSpeaking(false)
+        },
+    });
+
+    const createAudioBlob = (audio) => {
+        const wavBuffer = utils.encodeWAV(audio);
+        return new Blob([wavBuffer], {type: 'audio/wav'});
+    };
+
+    const validate = async (data) => {
+        const decodedData = await new AudioContext().decodeAudioData(await data.arrayBuffer());
+        const duration = decodedData.duration;
+        const minDuration = 0.4;
+
+        if (duration < minDuration) throw new Error(`Duration is ${duration}s, which is less than minimum of ${minDuration}s`);
+    };
+
+    const createBody = (data) => {
+        const formData = new FormData();
+        formData.append("audio", data, "audio.wav");
+        return formData;
+    };
+
+    const processAudio = async (audio) => {
+        const blob = createAudioBlob(audio);
+        await validate(blob);
+        sendData(blob);
+    };
+
+    const sendData = (blob) => {
+        setIsProcessing(true);
+        sendAudioData(blob)
+    };
+
+    const sendAudioData = (blob) => {
+        return fetch("http://localhost:8000/modify", {
+            method: "POST",
+            body: createBody(blob),
+            headers: {
+                'document': currentDocument
+            }
+        })
+            .then(handleResponse)
+            .then(handleSuccess)
+            .catch(handleError);
+    };
+
+    const handleResponse = (res) => {
+        if (!res.ok) {
+            return res.text().then(error => {
+                throw new Error(error);
+            });
+        }
+
+        return res.json();
+    };
+
+    const handleSuccess = (newDocument) => {
+        setCurrentDocument(newDocument);
+        setIsProcessing(false);
+    };
+
+    const handleError = (error) => {
+        console.log(`error encountered: ${error.message}`);
+        setIsProcessing(false);
+    };
 
     return (
         <Container size={700} className={classes.inner}>
-            <Title
+            {!isSpeaking && !isProcessing && <Title
+                variant="gradient"
+                gradient={{from: 'indigo', to: 'cyan', deg: 45}}
                 order={2}
                 size="h1"
                 sx={(theme) => ({fontFamily: `Greycliff CF, ${theme.fontFamily}`})}
@@ -34,9 +125,14 @@ export function Feedback() {
                 className={classes.title}
             >
                 Provide Your Feedback
-            </Title>
+            </Title>}
 
-            <Text fz="md">{document}</Text>
+            <Container size={100}>
+                {isSpeaking && <Loader size="xl" variant="bars"/>}
+                {isProcessing && <Loader size="xl"/>}
+            </Container>
+
+            <Text fz="md">{currentDocument}</Text>
         </Container>
     );
 }
